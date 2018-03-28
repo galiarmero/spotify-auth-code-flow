7ca9e04ffa6707b884177a439913e45df7bbb1ff
Gali Armero <galiarmero.io@gmail.com>
2018-03-28 17:37:33 +0800

    Get client config from a separate json file

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const querystring = require('querystring');
+const config = require('./config.json')
 const app = express();
-
-const CLIENT_ID = '<enter client_id>';
-const CLIENT_SECRET = '<enter client_secret>';
-const REDIRECT_URI = 'http://localhost:8888/callback';
-
 
 const generateRandomString = function(length) {
     let text = '';
@@ -25,9 +21,9 @@ app.get('/login', function(req, res) {
 
     res.redirect('https://accounts.spotify.com/authorize?' +
                     querystring.stringify({
-                        client_id: CLIENT_ID,
+                        client_id: config.CLIENT_ID,
                         response_type: 'code',
-                        redirect_uri: REDIRECT_URI,
+                        redirect_uri: config.REDIRECT_URI,
                         state: state,
                         scope: scope,
                         show_dialog: true
